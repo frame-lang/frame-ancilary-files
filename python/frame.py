29231589255b2052d3a8a5770ba1b37fe8056796
Mark Truluck <mark@frame-lang.org>
2021-03-16 07:02:40 -0700

    Update frame.py

--- python/frame.py
+++ python/frame.py
@@ -2,10 +2,10 @@
 
 class FrameEvent:
 
-    def __init__(self, message, params):
-        self.message = message
-        self.params = params
-        self.ret = None
+    def __init__(self, message, parameters):
+        self._message = message
+        self._parameters = parameters
+        self._return = None
         
 # ----------------------------------------- #
 
