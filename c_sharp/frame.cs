5d3c5fed68eb9db2a74d378b6513f8539ba71694
Mark Truluck <mark@frame-lang.org>
2021-03-16 06:51:03 -0700

    Create frame.cs

---
+++ c_sharp/frame.cs
@@ -0,0 +1,64 @@
+
+public class FrameEvent {
+	public FrameEvent(string msg, Dictionary<string,object> parameters) {
+		this.Msg = msg;
+		this.Parameters = parameters;
+	}
+	public string Msg;
+	public Dictionary<string,Object> Parameters;
+	public Object Return;
+}
+
+public delegate void FrameState(FrameEvent e);
+
+public class StateContext {
+	public StateContext(FrameState state) {
+		this.state = state;
+	}
+
+	public void addStateArg(string name, object value) {
+		stateArgs.Add(name,value);
+	}
+
+	public void setStateArg(string name, object value) {
+		stateArgs[name] = value;
+	}
+	
+	public object getStateArg(string name) {
+		return stateArgs[name];
+	}
+	
+	public void addStateVar(string name, object value) {
+		stateVars.Add(name,value);
+	}
+
+	public void setStateVar(string name, object value) {
+		stateVars[name] = value;
+	}
+	
+	public object getStateVar(string name) {
+		return stateVars[name];
+	}
+
+	public void addEnterArg(string name, object value) {
+		enterArgs.Add(name,value);
+	}
+
+	public void setEnterArg(string name, object value) {
+		enterArgs[name] = value;
+	}
+	
+	public object getEnterArg(string name) {
+		return enterArgs[name];
+	}
+	
+	public Dictionary<string,object> getEnterArgs() {
+		return enterArgs;
+	}
+
+	Dictionary<string,object> stateArgs = new Dictionary<string, object>();
+	Dictionary<string,object> stateVars = new Dictionary<string, object>();
+	Dictionary<string,object> enterArgs = new Dictionary<string, object>();
+	public FrameState state;
+
+}
